f38267ae5bbe328d43b57f4ffa8b676d030ef680
AlexLenitschek <alexander.lenitschek@gmx.de>
2025-02-25 20:22:52 +0100

    Projektdateien hinzufügen.

---
+++ WorkingWithEFCore/Program.cs
@@ -0,0 +1,5 @@
+﻿using Northwind.EntityModels; // To use Northwind.
+
+using NorthwindDb db = new();
+WriteLine($"Provider: {db.Database.ProviderName}");
+// The use of using ensures that the database context gets disposed after use.
