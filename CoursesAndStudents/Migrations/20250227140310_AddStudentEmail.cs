52104aabccb28f6c0cdbf5871081351b063a2143
AlexLenitschek <alexander.lenitschek@gmx.de>
2025-02-27 21:11:50 +0100

    Adding Migration stuff to chapter10

---
+++ CoursesAndStudents/Migrations/20250227140310_AddStudentEmail.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoursesAndStudents.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Students",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1,
+                column: "Email",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 2,
+                column: "Email",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 3,
+                column: "Email",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Students");
+        }
+    }
+}
